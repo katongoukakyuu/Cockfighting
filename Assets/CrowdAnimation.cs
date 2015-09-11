09cc60e7cf5cafb7d67759452b1809c439355597
Makr Duque <markrduque@gmail.com>
2015-09-12 02:19:51 +0800

    More Arena Stuff

---
+++ Assets/CrowdAnimation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrowdAnimation : MonoBehaviour {
+
+	public Animator crowdAnimation;
+
+	public int animTrack = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(animTrack == 1)
+		{
+			crowdAnimation.SetBool("isClapping", true);
+		}
+
+		if(animTrack == 2)
+		{
+			crowdAnimation.SetBool("isIdleStance",true);
+		}
+	
+	}
+}
